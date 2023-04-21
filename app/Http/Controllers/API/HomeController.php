53ef4fe520111c9457e5ac62b2b31b2b5c3067fb
egentric <85742839+egentric@users.noreply.github.com>
2023-04-21 12:29:27 +0200

    commit 24-04-23 dashboard

---
+++ app/Http/Controllers/API/HomeController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $home = DB::table('appmail_categories', 'appmail_contacts', 'users')
+            ->get()
+            ->toArray();
+
+        // On retourne les informations de la table commentaire en JSON
+        return response()->json([
+            'status' => 'Success',
+            'data' => $home,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
